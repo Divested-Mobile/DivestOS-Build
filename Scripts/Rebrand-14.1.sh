f810d0691dde174e877f466a3894e98d83c07ed5
Tad <tad@spotco.us>
2017-10-18 10:00:05 -0400

    Fixes and remove boot anim

--- Scripts/Rebrand-14.1.sh
+++ Scripts/Rebrand-14.1.sh
@@ -5,13 +5,15 @@
 
 base="/mnt/Drive-1/Development/Other/Android_ROMs/Build/LineageOS-14.1/"
 
+echo "Rebranding..."
+
 cd $base"packages/apps/Settings"
 sed -i '/.*cmlicense_title/s/LineageOS/DivestOS/' res/values*/cm_strings.xml
 sed -i '/.*cmupdate_settings_title/s/LineageOS/DivestOS/' res/values*/cm_strings.xml
 sed -i '/.*mod_version/s/LineageOS/DivestOS/' res/values*/cm_strings.xml
 
 cd $base"packages/apps/SetupWizard"
-sed -i 's|http://lineageos.org/legal|https://divestos.xyz/pages/legal/pp.html|' com/cyanogenmod/setupwizard/LineageSettingsActivity.java;
+sed -i 's|http://lineageos.org/legal|https://divestos.xyz/pages/legal/pp.html|' src/com/cyanogenmod/setupwizard/LineageSettingsActivity.java;
 sed -i '/.*setup_services/s/LineageOS/DivestOS/' res/values*/strings.xml
 sed -i '/.*services_explanation/s/LineageOS/DivestOS/' res/values*/strings.xml
 
@@ -19,6 +21,9 @@ cd $base"packages/apps/Updater"
 sed -i 's|>LineageOS|>DivestOS|' res/values*/strings.xml
 
 cd $base"vendor/cm"
-sed -i 's|http://lineageos.org/legal|https://divestos.xyz/pages/legal/tos.html|' config/common.mk;
+sed -i 's|http://lineageos.org/legal|https://divestos.xyz/pages/about.html|' config/common.mk;
 #sed -i '/.*ZIPFILE=/s/lineage/divestos/' build/envsetup.sh
 #TODO: Boot animation
+
+cd $base
+echo "Rebranding complete!"
