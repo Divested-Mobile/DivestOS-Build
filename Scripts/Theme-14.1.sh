28b054dd2de43c4a98eb88ae7160d4a88a41dee7
Tad <tad@spotco.us>
2017-10-18 19:08:22 -0400

    Theme more stuff

--- Scripts/Theme-14.1.sh
+++ Scripts/Theme-14.1.sh
@@ -7,25 +7,41 @@ base="/mnt/Drive-1/Development/Other/Android_ROMs/Build/LineageOS-14.1/"
 
 echo "Applying theme..."
 
+export themeOverride50="fbe9e7"
+export themeOverride100="ffccbc"
+export themeOverride200="ffab91"
+export themeOverride300="ff8a65"
+export themeOverride500="ff5722"
+export themeOverride700="e64a19"
+
 cd $base"frameworks/base"
-sed -i 's/#ffe0f2f1/#fffbe9e7/' core/res/res/values/colors_material.xml #50
-sed -i 's/#ffb2dfdb/#ffffccbc/' core/res/res/values/colors_material.xml #100
-sed -i 's/#ff80cbc4/#ffffab91/' core/res/res/values/colors_material.xml #200
-sed -i 's/#ff4db6ac/#ffff8a65/' core/res/res/values/colors_material.xml #300
-sed -i 's/#ff009688/#ffff5722/' core/res/res/values/colors_material.xml #500
-sed -i 's/#ff00796b/#ffe64a19/' core/res/res/values/colors_material.xml #700
+sed -i "s/#ffe0f2f1/#ff$themeOverride50/" core/res/res/values/colors_material.xml
+sed -i "s/#ffb2dfdb/#ff$themeOverride100/" core/res/res/values/colors_material.xml
+sed -i "s/#ff80cbc4/#ff$themeOverride200/" core/res/res/values/colors_material.xml
+sed -i "s/#ff4db6ac/#ff$themeOverride300/" core/res/res/values/colors_material.xml
+sed -i "s/#ff009688/#ff$themeOverride500/" core/res/res/values/colors_material.xml
+sed -i "s/#ff00796b/#ff$themeOverride700/" core/res/res/values/colors_material.xml
 
 cd $base"packages/apps/GmsCore"
-sed -i 's/#ff7fcac3/#ffff8a65/' microg-ui-tools/src/main/res/values/colors.xml #300
-sed -i 's/#ff009688/#ffff5722/' microg-ui-tools/src/main/res/values/colors.xml #500
+sed -i "s/#ff7fcac3/#ff$themeOverride300/" microg-ui-tools/src/main/res/values/colors.xml
+sed -i "s/#ff009688/#ff$themeOverride500/" microg-ui-tools/src/main/res/values/colors.xml
 
 #cd $base"packages/apps/Settings"
-#sed -i 's///' res/values/colors.xml
+#sed -i "s///" res/values/colors.xml
 #TODO: Fix 'Storage'
 
+cd $base"packages/apps/Trebuchet"
+sed -i "s/009688/$themeOverride500/" res/values/*colors.xml
+mogrify -format png -fill "#$themeOverride500" -opaque "#009688" res/drawable*/cling_bg.9.png
+
 cd $base"packages/inputmethods/LatinIME"
-sed -i 's/#80CBC4/#ffab91/' java/res/values/colors.xml #200
-sed -i 's/#4DB6AC/#ff8a65/' java/res/values/colors.xml #300
+sed -i "s/#80CBC4/#$themeOverride200/" java/res/values/colors.xml
+sed -i "s/#4DB6AC/#$themeOverride300/" java/res/values/colors.xml
+mogrify -format png -fill "#$themeOverride100" -opaque "#b2dfdb" java/res/drawable*/*lxx*.png
+mogrify -format png -fill "#$themeOverride200" -opaque "#80cbc4" java/res/drawable*/*lxx*.png
+mogrify -format png -fill "#$themeOverride300" -opaque "#4db6ac" java/res/drawable*/*lxx*.png
+mogrify -format png -fill "#$themeOverride300" -opaque "#7fcac3" java/res/drawable*/*lxx*.png
+mogrify -format png -fill "#$themeOverride400" -opaque "#26a69a" java/res/drawable*/*lxx*.png
 
 cd $base
 echo "Applied theme!"
