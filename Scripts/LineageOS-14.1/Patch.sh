397ab78e21403d22127fd080cfb8178f5bb976e2
Tad <tad@spotco.us>
2017-12-19 00:18:10 -0500

    Remove some broken patches

--- Scripts/LineageOS-14.1/Patch.sh
+++ Scripts/LineageOS-14.1/Patch.sh
@@ -72,7 +72,6 @@ patch -p1 < $patches"android_external_sqlite/0001-Secure_Delete.patch" #Enable s
 
 enter "frameworks/base"
 git revert 0326bb5e41219cf502727c3aa44ebf2daa19a5b3 #re-enable doze on devices without gms
-git fetch https://review.lineageos.org/LineageOS/android_frameworks_base refs/changes/75/151975/38 && git cherry-pick FETCH_HEAD #network traffic
 sed -i 's/DEFAULT_MAX_FILES = 1000;/DEFAULT_MAX_FILES = 0;/' services/core/java/com/android/server/DropBoxManagerService.java; #Disable DropBox
 sed -i 's/com.android.messaging/org.smssecure.smssecure/' core/res/res/values/config.xml; #Change default SMS app to Silence
 sed -i 's|config_permissionReviewRequired">false|config_permissionReviewRequired">true|' core/res/res/values/config.xml;
@@ -96,7 +95,6 @@ awk -i inplace '!/com.android.internal.R.bool.config_permissionReviewRequired/' 
 
 enter "packages/apps/CMParts"
 rm -rf src/org/cyanogenmod/cmparts/cmstats/ res/xml/anonymous_stats.xml res/xml/preview_data.xml #Nuke part of CMStats
-git fetch https://review.lineageos.org/LineageOS/android_packages_apps_CMParts refs/changes/15/113415/26 && git cherry-pick FETCH_HEAD #network traffic
 sed -i 's|config_showWeatherMenu">true|config_showWeatherMenu">false|' res/values/config.xml; #Disable Weather
 patch -p1 < $patches"android_packages_apps_CMParts/0001-Remove_Analytics.patch" #Remove the rest of CMStats
 patch -p1 < $patches"android_packages_apps_CMParts/0002-Reduced_Resolution.patch" #Allow reducing resolution to save power
@@ -127,9 +125,6 @@ enter "packages/apps/IchnaeaNlpBackend"
 sed -i 's|$(OUT_DIR)/target/|$(PWD)/$(OUT_DIR)/target/|' Android.mk;
 sed -i 's/compileSdkVersion 23/compileSdkVersion 25/' build.gradle;
 sed -i 's/buildToolsVersion "23.0.2"/buildToolsVersion "25.0.3"/' build.gradle;
-
-enter "packages/apps/Jelly"
-git apply --3way $patches"android_packages_apps_Jelly/182322-3.patch" #Add option to remove identifying headers
 
 enter "packages/apps/PackageInstaller"
 patch -p1 < $patches"android_packages_apps_PackageInstaller/64d8b44.diff" #Fix an issue with Permission Review
@@ -188,7 +183,6 @@ sed -i 's/messaging/Silence/' config/telephony.mk; #Replace AOSP Messaging app w
 #sed -i 's/mka bacon/mka bacon target-files-package dist/' build/envsetup.sh; #Create target-files for incrementals
 
 enter "vendor/cmsdk"
-git fetch https://review.lineageos.org/LineageOS/cm_platform_sdk refs/changes/21/148321/14 && git cherry-pick FETCH_HEAD #network traffic
 awk -i inplace '!/WeatherManagerServiceBroker/' cm/res/res/values/config.xml; #Disable Weather
 cp $patches"cm_platform_sdk/profile_default.xml" cm/res/res/xml/profile_default.xml; #Replace default profiles with *way* better ones
 #patch -p1 < $patches"cm_platform_sdk/0001-Radio.patch" #Add a QS tile to control radio power
