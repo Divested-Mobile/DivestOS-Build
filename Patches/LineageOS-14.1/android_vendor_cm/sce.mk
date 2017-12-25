406a4ebf6e2973fc2cf791eb1ae02ca7920fc299
Tad <tad@spotco.us>
2017-12-24 20:59:00 -0500

    Disable Fennec DOS shim

--- Patches/LineageOS-14.1/android_vendor_cm/sce.mk
+++ Patches/LineageOS-14.1/android_vendor_cm/sce.mk
@@ -16,5 +16,10 @@ PRODUCT_PACKAGES += \
     FDroidPrivilegedExtension
 
 # Browser
-PRODUCT_PACKAGES += \
-    FennecDOS
+# XXX: THIS DOESN'T WORK
+# This is a shim, it is intended that F-Droid will update on first run to the real version of Fennec DOS
+# However it doesn't work for whatever reason
+# and guess what? There is no documentation about updating system apps. Searching is useless and filled with spam.
+# I'm sure Google Play Services does some special bullshit handling of system apps, but we're not allowed to know about that.
+#PRODUCT_PACKAGES += \
+#    FennecDOS
