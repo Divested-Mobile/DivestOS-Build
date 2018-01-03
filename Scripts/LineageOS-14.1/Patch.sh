9e24a92b8918eefdd682dc49d986f85979bd9bed
Tad <tad@spotco.us>
2018-01-03 15:48:18 -0500

    Build fix

--- Scripts/LineageOS-14.1/Patch.sh
+++ Scripts/LineageOS-14.1/Patch.sh
@@ -46,7 +46,7 @@ wget https://spotco.us/hosts -N #XXX: /hosts is built from non-commercial use fi
 
 #Accept all SDK licences, not normally needed but Gradle managed apps fail without it
 mkdir -p "$ANDROID_HOME/licenses"
-echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
+echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55\nd56f5187479451eabf01fb78af6dfcb131a6481e" > "$ANDROID_HOME/licenses/android-sdk-license"
 echo -e "\n84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_HOME/licenses/android-sdk-preview-license"
 #
 #END OF PREPRATION
