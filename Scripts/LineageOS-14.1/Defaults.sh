e918cec33baeac4391aaade969babd847a7c02d2
Tad <tad@spotco.us>
2018-01-03 13:02:20 -0500

    Fixup 7446b2d304d0aa8b3a16f3dff97decd15da9adc5

--- Scripts/LineageOS-14.1/Defaults.sh
+++ Scripts/LineageOS-14.1/Defaults.sh
@@ -19,38 +19,38 @@
 
 echo "Changing default settings..."
 
-enter $base"frameworks/base"
+enter "frameworks/base"
 sed -i '0,/wifi,cell,battery/s/wifi,cell,battery,dnd,flashlight,rotation,bt,airplane/wifi,cell,bt,dnd,flashlight,rotation,battery,profiles,location,airplane,saver,hotspot,nfc/' packages/SystemUI/res/values/config.xml; #Default quick tiles
 #sed -i 's|config_longPressOnHomeBehavior">2|config_longPressOnHomeBehavior">0|' core/res/res/values/config.xml; #Set long press home to do nothing
 #sed -i 's|config_doubleTapOnHomeBehavior">0|config_doubleTapOnHomeBehavior">8|' core/res/res/values/config.xml; #Set double tap home to switch to last app
 
-enter $base"packages/apps/Dialer"
+enter "packages/apps/Dialer"
 sed -i 's/FLP_DEFAULT = FLP_GOOGLE;/FLP_DEFAULT = FLP_OPENSTREETMAP;/' src/com/android/dialer/lookup/LookupSettings.java; #Change FLP to OpenStreetMap
 sed -i 's/CMSettings.System.ENABLE_FORWARD_LOOKUP, 1)/CMSettings.System.ENABLE_FORWARD_LOOKUP, 0)/' src/com/android/dialer/lookup/LookupSettings.java; #Disable FLP
 sed -i 's/CMSettings.System.ENABLE_PEOPLE_LOOKUP, 1)/CMSettings.System.ENABLE_PEOPLE_LOOKUP, 0)/' src/com/android/dialer/lookup/LookupSettings.java; #Disable PLP
 sed -i 's/CMSettings.System.ENABLE_REVERSE_LOOKUP, 1)/CMSettings.System.ENABLE_REVERSE_LOOKUP, 0)/' src/com/android/dialer/lookup/LookupSettings.java; #Disable RLP
 
-enter $base"packages/apps/FDroid"
+enter "packages/apps/FDroid"
 sed -i 's|DEFAULT_ROOTED = true;|DEFAULT_ROOTED = false;|' app/src/main/java/org/fdroid/fdroid/Preferences.java; #Hide root apps
 sed -i '/string\/rooted/!b;n;s/defaultValue="true"/defaultValue="false"/' app/src/main/res/xml/preferences.xml;
 sed -i 's|DEFAULT_HIDE_ANTI_FEATURE_APPS = false;|DEFAULT_HIDE_ANTI_FEATURE_APPS = true;|' app/src/main/java/org/fdroid/fdroid/Preferences.java; #Hide anti-feature apps
 sed -i '/string\/hide_anti_feature_apps/!b;n;s/defaultValue="false"/defaultValue="true"/' app/src/main/res/xml/preferences.xml;
 
-enter $base"packages/apps/Jelly"
+enter "packages/apps/Jelly"
 sed -i 's|default_suggestion_provider">GOOGLE|default_suggestion_provider">NONE|' app/src/main/res/values/strings.xml; #Disable search suggestions
 sed -i 's|KEY_LOCATION, true|KEY_LOCATION, false|' app/src/main/java/org/lineageos/jelly/utils/PrefsUtils.java; #Disable location
 sed -i 's|KEY_DO_NOT_TRACK, false|KEY_DO_NOT_TRACK, true|' app/src/main/java/org/lineageos/jelly/utils/PrefsUtils.java; #Enable do not track
 sed -i 's|KEY_SAVE_FORM_DATA, true|KEY_SAVE_FORM_DATA, false|' app/src/main/java/org/lineageos/jelly/utils/PrefsUtils.java; #Disable form data saving
 sed -i 's|KEY_REMOVE_IDENTIFYING_HEADERS, false|KEY_REMOVE_IDENTIFYING_HEADERS, true|' app/src/main/java/org/lineageos/jelly/utils/PrefsUtils.java; #Remove certain headers
 
-enter $base"packages/apps/Nfc"
+enter "packages/apps/Nfc"
 sed -i 's/static final boolean NFC_ON_DEFAULT = true;/static final boolean NFC_ON_DEFAULT = false;/' src/com/android/nfc/NfcService.java; #Disable NFC
 sed -i 's/static final boolean NDEF_PUSH_ON_DEFAULT = true;/static final boolean NDEF_PUSH_ON_DEFAULT = false;/' src/com/android/nfc/NfcService.java; #Disable NDEF Push
 
-enter $base"packages/apps/Settings"
+enter "packages/apps/Settings"
 sed -i 's/Settings.Secure.WEB_ACTION_ENABLED, 1/Settings.Secure.WEB_ACTION_ENABLED, 0/' src/com/android/settings/applications/ManageDomainUrls.java; #Disable "Instant Apps"
 
-enter $base"packages/apps/Trebuchet"
+enter "packages/apps/Trebuchet"
 sed -i 's|homescreen_search_default">true|homescreen_search_default">false|' res/values/preferences_defaults.xml; #Disable search
 sed -i 's|drawer_compact_default">false|drawer_compact_default">true|' res/values/preferences_defaults.xml; #Enable compact view
 sed -i 's|use_scroller_default">true|use_scroller_default">false|' res/values/preferences_defaults.xml; #Hide scroller
--- Scripts/LineageOS-14.1/Theme.sh
+++ Scripts/LineageOS-14.1/Theme.sh
@@ -33,7 +33,7 @@ export themeOverride700="E64A19" #Deep Orange 700
 #export themeOverride500="2196F3" #Blue 500
 #export themeOverride700="1976D2" #Blue 700
 
-enter $base"frameworks/base"
+enter "frameworks/base"
 sed -i "s/#ffe0f2f1/#ff$themeOverride50/" core/res/res/values/colors_material.xml
 sed -i "s/#ffb2dfdb/#ff$themeOverride100/" core/res/res/values/colors_material.xml
 sed -i "s/#ff80cbc4/#ff$themeOverride200/" core/res/res/values/colors_material.xml
@@ -42,27 +42,27 @@ sed -i "s/#ff009688/#ff$themeOverride500/" core/res/res/values/colors_material.x
 sed -i "s/#ff00796b/#ff$themeOverride700/" core/res/res/values/colors_material.xml
 sed -i "s/#fff4511e/#ffe53935/" core/res/res/values/colors.xml
 
-enter $base"packages/apps/CMParts"
+enter "packages/apps/CMParts"
 sed -i "s/#ff009688/#ff$themeOverride500/" res/values/colors.xml
 
-enter $base"packages/apps/GmsCore"
+enter "packages/apps/GmsCore"
 sed -i "s/#ff7fcac3/#ff$themeOverride300/" microg-ui-tools/src/main/res/values/colors.xml
 sed -i "s/#ff009688/#ff$themeOverride500/" microg-ui-tools/src/main/res/values/colors.xml
 
-enter $base"packages/apps/Settings"
+enter "packages/apps/Settings"
 sed -i "s/#ff009688/#ff$themeOverride500/" res/values/styles.xml
 #TODO: Fix: Storage, Profiles
 
-enter $base"packages/apps/Trebuchet"
+enter "packages/apps/Trebuchet"
 sed -i "s/009688/$themeOverride500/" res/values/*colors.xml
 sed -i "s/009688/$themeOverride500/" WallpaperPicker/res/values/colors.xml
 mogrify -format png -fill "#$themeOverride500" -opaque "#009688" -fuzz 10% res/drawable*/cling_bg.9.png
 #TODO: Fix: Open app icon
 
-enter $base"packages/apps/Updater"
+enter "packages/apps/Updater"
 sed -i "s/#ff009688/#ff$themeOverride500/" res/values/colors.xml
 
-enter $base"packages/inputmethods/LatinIME"
+enter "packages/inputmethods/LatinIME"
 sed -i "s/#80CBC4/#$themeOverride200/" java/res/values/colors.xml
 sed -i "s/#4DB6AC/#$themeOverride300/" java/res/values/colors.xml
 mogrify -format png -fill "#$themeOverride100" -opaque "#b2dfdb" -fuzz 10% java/res/drawable*/*lxx*.png
