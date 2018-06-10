3ea4b55e70edbbd78ffa46489b933563258d0f77
Tad <tad@spotco.us>
2018-06-10 17:04:58 -0400

    Tweaks and add a script for creating signing keys

---
+++ Scripts/Generate_Signing_Keys.sh
@@ -0,0 +1,11 @@
+export desc='/O=Divested Computing, Inc./CN=DivestOS/emailAddress=support@divestos.xyz';
+export type='rsa'; #Options: rsa, ec
+
+$base/development/tools/make_key extra "$desc" "$type";
+$base/development/tools/make_key media "$desc" "$type";
+$base/development/tools/make_key platform "$desc" "$type";
+$base/development/tools/make_key releasekey "$desc" "$type";
+$base/development/tools/make_key shared "$desc" "$type";
+$base/development/tools/make_key verity "$desc" "$type";
+
+echo "Please copy created keys to your signing keys directory. Keep them safe!";
