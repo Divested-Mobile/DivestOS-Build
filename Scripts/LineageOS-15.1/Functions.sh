e259807d447873c378e53bcfd55eff58d1ec6ceb
Tad <tad@spotco.us>
2018-06-13 07:23:59 -0400

    Manifest cleanup

--- Scripts/LineageOS-15.1/Functions.sh
+++ Scripts/LineageOS-15.1/Functions.sh
@@ -18,7 +18,7 @@
 #Last verified: 2018-04-27
 
 patchAllKernels() {
-	startPatcher "kernel_fairphone_msm8974 kernel_google_marlin kernel_google_msm kernel_htc_flounder kernel_htc_msm8974 kernel_huawei_angler kernel_lge_bullhead kernel_lge_g3 kernel_lge_hammerhead kernel_lge_mako kernel_lge_msm8974 kernel_lge_msm8996 kernel_moto_shamu kernel_motorola_msm8992 kernel_motorola_msm8996 kernel_nextbit_msm8992 kernel_oppo_msm8974 kernel_samsung_msm8974";
+	startPatcher "kernel_google_marlin kernel_google_msm kernel_htc_flounder kernel_htc_msm8974 kernel_huawei_angler kernel_lge_bullhead kernel_lge_g3 kernel_lge_hammerhead kernel_lge_mako kernel_lge_msm8974 kernel_lge_msm8996 kernel_moto_shamu kernel_motorola_msm8992 kernel_motorola_msm8996 kernel_nextbit_msm8992 kernel_oppo_msm8974 kernel_samsung_msm8974";
 }
 export -f patchAllKernels;
 
@@ -62,8 +62,9 @@ buildAll() {
 	brunch lineage_flo-user;
 	brunch lineage_flounder-user;
 	brunch lineage_griffin-user;
-	#brunch lineage_h850-userdebug;
+	brunch lineage_h850-userdebug;
 	brunch lineage_hammerhead-user; #broken upstream - fatal error: 'linux/msm_audio_calibration.h' file not found
+	brunch lineage_klte-user;
 	brunch lineage_marlin-user;
 	brunch lineage_m8-user;
 	brunch lineage_sailfish-user;
