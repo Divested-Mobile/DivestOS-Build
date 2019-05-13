899812864feafd0f329ff4032596d44b270e5943
Tad <tad@spotco.us>
2019-05-13 17:21:55 -0400

    Update CVE patchers

--- Scripts/LineageOS-16.0/Functions.sh
+++ Scripts/LineageOS-16.0/Functions.sh
@@ -64,7 +64,6 @@ patchWorkspace() {
 	if [ "$DOS_MALWARE_SCAN_ENABLED" = true ]; then scanForMalware false "$DOS_PREBUILT_APPS $DOS_BUILD_BASE/build $DOS_BUILD_BASE/device $DOS_BUILD_BASE/vendor/lineage"; fi;
 
 	source build/envsetup.sh;
-	#repopick -it bt-sbc-hd-dualchannel-pie;
 	repopick 245252; #update webview
 
 	source "$DOS_SCRIPTS/Patch.sh";
