ae112ef6141ef890ddf544ce71a4ab3ffddf1f6f
Tad <tad@spotco.us>
2020-06-07 06:27:34 -0400

    Add an AOSP CVE patch list
    Made with a scraper
    May contain many errors

---
+++ Misc/aosp-cves/gen_cve_list.sh
@@ -0,0 +1,105 @@
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-06-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-05-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-04-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-03-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-02-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-01-01" >> cve_list.txt
+
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2020-06-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2020-05-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2020-04-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2020-03-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2020-02-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2020-01-01" >> cve_list.txt
+
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-12-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-11-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-10-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-09-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-08-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-07-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-06-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-05-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-04-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-03-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-02-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2019-01-01" >> cve_list.txt
+
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-12-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-11-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-10-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-09-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-08-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-07-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-06-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-05-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-04-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-03-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-02-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2019-01-01" >> cve_list.txt
+
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-12-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-11-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-10-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-09-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-08-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-07-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-06-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-05-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-04-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-03-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-02-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2018-01-01" >> cve_list.txt
+
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-12-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-11-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-10-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-09-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-08-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-07-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-06-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-05-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-04-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-03-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-02-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2018-01-01" >> cve_list.txt
+
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-12-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-11-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-10-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-09-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-08-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-07-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-06-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-05-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-04-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-03-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-02-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2017-01-01" >> cve_list.txt
+
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2017-12-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2017-11-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2017-10-01" >> cve_list.txt
+
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-12-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-11-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-10-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-09-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-08-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-07-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-06-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-05-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-04-02" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-03-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-02-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2016-01-01" >> cve_list.txt
+
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2015-12-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2015-11-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2015-10-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2015-09-01" >> cve_list.txt
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2015-08-01" >> cve_list.txt
+
+# clean a bit
+# sort: java -jar $DOS_BINARY_PATCHER sort cve_list.txt  >> cve_list-sorted.txt
+# clean some more
