dc3cf3405fcc2fdc4ee0543a8b068876ed409b5a
Tad <tad@spotco.us>
2020-07-19 16:10:48 -0400

    Small updates
    - Fix USB breakage

--- Scripts/LineageOS-15.1/Functions.sh
+++ Scripts/LineageOS-15.1/Functions.sh
@@ -105,7 +105,7 @@ patchWorkspace() {
 	if [ "$DOS_MALWARE_SCAN_ENABLED" = true ]; then scanForMalware false "$DOS_PREBUILT_APPS $DOS_BUILD_BASE/build $DOS_BUILD_BASE/device $DOS_BUILD_BASE/vendor/lineage"; fi;
 
 	source build/envsetup.sh;
-	repopick -i 280662 280664; #O_asb_2020-07
+	repopick -i 280662 280664 281415; #O_asb_2020-07
 
 	export DOS_GRAPHENE_MALLOC=false; #patches apply, compile fails
 
