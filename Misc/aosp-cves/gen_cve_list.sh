556a8529e6365de302796e2a36c4584ea5283180
Tad <tad@spotco.us>
2020-09-08 17:02:41 -0400

    Update AOSP CVE list to September patches
    - This will need to be re run after more links are added as usual

--- Misc/aosp-cves/gen_cve_list.sh
+++ Misc/aosp-cves/gen_cve_list.sh
@@ -1,3 +1,4 @@
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-09-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-08-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-07-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-06-01" >> cve_list.txt
@@ -7,6 +8,7 @@ java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulle
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-02-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2020-01-01" >> cve_list.txt
 
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2020-09-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2020-08-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2020-07-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2020-06-01" >> cve_list.txt
