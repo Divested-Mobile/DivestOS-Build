731e0e995cb24bae206d6e1eb2970a0305146e43
Tad <tad@spotco.us>
2021-05-07 21:15:04 -0400

    Update CVE patchers

--- Scripts/LineageOS-18.1/Functions.sh
+++ Scripts/LineageOS-18.1/Functions.sh
@@ -101,9 +101,7 @@ export -f buildAll;
 patchWorkspace() {
 	if [ "$DOS_MALWARE_SCAN_ENABLED" = true ]; then scanForMalware false "$DOS_PREBUILT_APPS $DOS_BUILD_BASE/build $DOS_BUILD_BASE/device $DOS_BUILD_BASE/vendor/lineage"; fi;
 
-	source build/envsetup.sh;
-	repopick -i 300595; #system/netd: add missing override keyword
-	#repopick -it android-11.0.0_r37;
+	#source build/envsetup.sh;
 
 	source "$DOS_SCRIPTS/Patch.sh";
 	source "$DOS_SCRIPTS_COMMON/Copy_Keys.sh";
