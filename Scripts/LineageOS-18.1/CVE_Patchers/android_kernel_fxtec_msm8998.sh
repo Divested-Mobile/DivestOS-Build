48f35901c2ef6c576a3c37cd479494ce415293e6
Tad <tad@spotco.us>
2021-06-16 02:41:29 -0400

    Update CVE patchers

--- Scripts/LineageOS-18.1/CVE_Patchers/android_kernel_fxtec_msm8998.sh
+++ Scripts/LineageOS-18.1/CVE_Patchers/android_kernel_fxtec_msm8998.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "$DOS_BUILD_BASE""kernel/fxtec/msm8998"
+git apply $DOS_PATCHES_LINUX_CVES/0001-LinuxIncrementals/4.4/4.4.0272-0273.patch --exclude=Makefile
 git apply $DOS_PATCHES_LINUX_CVES/0007-Accelerated_AES/3.10+/0016.patch
 git apply $DOS_PATCHES_LINUX_CVES/0007-Accelerated_AES/3.10+/0020.patch
 git apply $DOS_PATCHES_LINUX_CVES/0008-Graphene-Kernel_Hardening/4.4/0002.patch
@@ -49,5 +50,5 @@ git apply $DOS_PATCHES_LINUX_CVES/CVE-2020-11608/4.4/0006.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2020-11608/^5.6.1/0001.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2020-14386/3.10-^4.4/0002.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2020-16119/^5.10/0002.patch
-editKernelLocalversion "-dos.p49"
+editKernelLocalversion "-dos.p50"
 cd "$DOS_BUILD_BASE"
