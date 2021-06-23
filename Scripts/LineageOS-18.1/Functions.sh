eb3e51e7e3211e25cb705f78b007fe1969e9d0f8
Tad <tad@spotco.us>
2021-06-23 13:00:43 -0400

    Small tweaks

--- Scripts/LineageOS-18.1/Functions.sh
+++ Scripts/LineageOS-18.1/Functions.sh
@@ -103,8 +103,9 @@ export -f buildAll;
 patchWorkspace() {
 	if [ "$DOS_MALWARE_SCAN_ENABLED" = true ]; then scanForMalware false "$DOS_PREBUILT_APPS $DOS_BUILD_BASE/build $DOS_BUILD_BASE/device $DOS_BUILD_BASE/vendor/lineage"; fi;
 
-	source build/envsetup.sh;
-	repopick -i 312738; #lineage-sdk: Mark all pending intents as immutable
+	#source build/envsetup.sh;
+	repopick -i 312861; #recorder intent improvement
+	#repopick -i 312866; #mata speaker mic fix
 
 	source "$DOS_SCRIPTS/Patch.sh";
 	source "$DOS_SCRIPTS_COMMON/Copy_Keys.sh";
