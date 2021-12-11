f31972f8c87c05663db33d2baf5c6c01dffd56e6
Tad <tad@spotco.us>
2021-12-11 13:14:15 -0500

    Update AOSP CVE list to December 2021
    Signed-off-by: Tad <tad@spotco.us>

--- Misc/aosp-cves/gen_cve_list.sh
+++ Misc/aosp-cves/gen_cve_list.sh
@@ -1,3 +1,4 @@
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2021-12-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2021-11-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2021-10-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2021-09-01" >> cve_list.txt
@@ -10,6 +11,7 @@ java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulle
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2021-02-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/2021-01-01" >> cve_list.txt
 
+java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2021-12-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2021-11-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2021-10-01" >> cve_list.txt
 java -jar $DOS_BINARY_PATCHER scraper "https://source.android.com/security/bulletin/pixel/2021-09-01" >> cve_list.txt
