e9f124b52d1ff37378adc87116dcfca77dc2aaa2
Tad <tad@spotco.us>
2022-08-27 15:14:07 -0400

    Fixup
    Signed-off-by: Tad <tad@spotco.us>

--- Scripts/Common/Tag_Verifier.sh
+++ Scripts/Common/Tag_Verifier.sh
@@ -15,7 +15,7 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <https://www.gnu.org/licenses/>.
 umask 0022;
-set -uo pipefail;
+#set -uo pipefail;
 source "$DOS_SCRIPTS_COMMON/Shell.sh";
 
 gpgVerifyGitTag() {
