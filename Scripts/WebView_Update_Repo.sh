fe95f700d82ccf9536c191f85dff4a8a4c04282c
Tad <tad@spotco.us>
2022-10-12 00:30:49 -0400

    Remove the CNE and DPM blobs + churn
    Just say no!
    
    Signed-off-by: Tad <tad@spotco.us>

--- Scripts/WebView_Update_Repo.sh
+++ Scripts/WebView_Update_Repo.sh
@@ -16,7 +16,7 @@
 umask 0022;
 set -uo pipefail;
 
-export version="106.0.5249.79-1";
+export version="106.0.5249.118-1";
 export PATH=$PATH:$HOME/Android/Sdk/build-tools/33.0.0;
 export webviewARM32="/mnt/dos/Repos/DivestOS_WebView/prebuilt/arm/webview.apk";
 export webviewARM64="/mnt/dos/Repos/DivestOS_WebView/prebuilt/arm64/webview.apk";
