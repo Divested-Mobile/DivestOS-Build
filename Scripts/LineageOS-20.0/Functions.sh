5f366227bdb3eca4ba3f6029ee50a2cb2df57354
Tad <tad@spotco.us>
2023-03-25 17:07:31 -0400

    cheryl: promote to 20.0
    Signed-off-by: Tad <tad@spotco.us>

--- Scripts/LineageOS-20.0/Functions.sh
+++ Scripts/LineageOS-20.0/Functions.sh
@@ -19,7 +19,7 @@ umask 0022;
 #Last verified: 2022-10-15
 
 patchAllKernels() {
-	startPatcher "kernel_essential_msm8998 kernel_fairphone_sdm632 kernel_fairphone_sm7225 kernel_fxtec_msm8998 kernel_google_gs101_private_gs-google kernel_google_gs201_private_gs-google kernel_google_msm-4.9 kernel_google_msm-4.14 kernel_google_redbull kernel_google_wahoo kernel_oneplus_msm8998 kernel_oneplus_sdm845 kernel_oneplus_sm7250 kernel_oneplus_sm8150 kernel_oneplus_sm8250 kernel_oneplus_sm8350 kernel_razer_sdm845 kernel_sony_sdm845 kernel_xiaomi_sdm845";
+	startPatcher "kernel_essential_msm8998 kernel_fairphone_sdm632 kernel_fairphone_sm7225 kernel_fxtec_msm8998 kernel_google_gs101_private_gs-google kernel_google_gs201_private_gs-google kernel_google_msm-4.9 kernel_google_msm-4.14 kernel_google_redbull kernel_google_wahoo kernel_oneplus_msm8998 kernel_oneplus_sdm845 kernel_oneplus_sm7250 kernel_oneplus_sm8150 kernel_oneplus_sm8250 kernel_oneplus_sm8350 kernel_razer_msm8998 kernel_razer_sdm845 kernel_sony_sdm845 kernel_xiaomi_sdm845";
 }
 export -f patchAllKernels;
 
@@ -60,6 +60,7 @@ buildAll() {
 	buildDevice cheeseburger verity;
 	buildDevice dumpling verity;
 	buildDevice mata verity; #unb
+	buildDevice cheryl verity;
 	#SD845
 	buildDevice fajita avb;
 	buildDevice enchilada avb;
