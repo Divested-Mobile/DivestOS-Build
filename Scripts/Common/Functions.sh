5f6a176e8cdb79354d08e518926e05e653ffa668
Tad <tad@spotco.us>
2023-07-13 10:39:30 -0400

    Fixup 89424768
    Signed-off-by: Tad <tad@spotco.us>

--- Scripts/Common/Functions.sh
+++ Scripts/Common/Functions.sh
@@ -403,16 +403,18 @@ processRelease() {
 		mkdir -vp $ARCHIVE/fastboot;
 		mkdir -vp $ARCHIVE/incrementals;
 
-		if [[ " ${DOS_GENERATE_DELTAS_DEVICES[@]} " =~ " ${DEVICE} " ]]; then
-			cp -v $OUT_DIR/$PREFIX-target_files.zip* $ARCHIVE/target_files/;
-			cp -v $OUT_DIR/$PREFIX-incremental_*.zip* $ARCHIVE/incrementals/ || true;
-		fi;
 		cp -v $OUT_DIR/$PREFIX-fastboot.zip* $ARCHIVE/fastboot/ || true;
-		cp -v $OUT_DIR/$PREFIX-ota.zip* $ARCHIVE/;
+		cp -v $OUT_DIR/$PREFIX-ota.zip* $ARCHIVE/ || true;
 		cp -v $OUT_DIR/$PREFIX-recovery.img* $ARCHIVE/ || true;
-
 		rename -- "-ota." "." $ARCHIVE/$PREFIX-ota.zip*;
-		if [[ " ${DOS_GENERATE_DELTAS_DEVICES[@]} " =~ " ${DEVICE} " ]]; then rename -- "-incremental_" "-" $ARCHIVE/incrementals/$PREFIX-incremental_*.zip*; fi;
+		if [ "$DOS_GENERATE_DELTAS" = true ];
+			if [[ " ${DOS_GENERATE_DELTAS_DEVICES[@]} " =~ " ${DEVICE} " ]]; then
+				cp -v $OUT_DIR/$PREFIX-target_files.zip* $ARCHIVE/target_files/ || true;
+				cp -v $OUT_DIR/$PREFIX-incremental_*.zip* $ARCHIVE/incrementals/ || true;
+				rename -- "-incremental_" "-" $ARCHIVE/incrementals/$PREFIX-incremental_*.zip* || true;
+			fi;
+		fi;
+
 		sync;
 
 		#Remove to make space for next build
