d34d67680565f8edcf5da27cab93cd4be0ca6440
Tad <tad@spotco.us>
2023-08-20 15:18:21 -0400

    Fixup removal of DebuggingRestrictionController
    Signed-off-by: Tad <tad@spotco.us>

--- Scripts/Common/Deblob.sh
+++ Scripts/Common/Deblob.sh
@@ -908,7 +908,7 @@ fi;
 deblobVendors; #Deblob entire vendor directory
 rm -rf frameworks/av/drm/mediadrm/plugins/clearkey; #Remove ClearKey
 #rm -rf frameworks/av/drm/mediacas/plugins/clearkey; #XXX: breaks protobuf inclusion
-#rm -rf packages/apps/Car/DebuggingRestrictionController || true; #Remove package that depends on Play Services #XXX: breaks compile
+rm -rf packages/apps/Car/DebuggingRestrictionController/* || true; #Remove package that depends on Play Services
 [[ -d vendor/samsung/nodevice ]] && rm -rf vendor/samsung/nodevice;
 #
 #END OF DEBLOBBING
