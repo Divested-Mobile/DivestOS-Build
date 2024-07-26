3827a096da014cc4f1f93678a174edea2195fcec
ryneeverett <ryneeverett@gmail.com>
2024-07-26 16:53:46 -0400

    Container: Find git config at XDG_CONFIG_HOME

--- Container/build-image-podman.sh
+++ Container/build-image-podman.sh
@@ -2,6 +2,6 @@
 
 # from https://android.googlesource.com/platform/build/+/master/tools/docker
 # Copy your host gitconfig, or create a stripped down version
-cp ~/.gitconfig gitconfig
+cp ~/.gitconfig gitconfig || cp $XDG_CONFIG_HOME/git/config gitconfig
 podman build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t android-build-fedora .
 rm gitconfig
