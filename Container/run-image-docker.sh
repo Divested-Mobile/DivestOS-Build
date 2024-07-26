82164037297994e57af3cf33605f5155c590be34
ryneeverett <ryneeverett@gmail.com>
2024-07-26 17:26:35 -0400

    Container: Store DivestOS directory in a volume
    This essentially serves as a cache of sources because otherwise all the
    downloaded source disappears when the container exits.

--- Container/run-image-docker.sh
+++ Container/run-image-docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mkdir -p $1
-docker run -it --rm -v $1:/home/$(id -un)/DivestOS/Builds -v divestos_ccache:/home/$(id -un)/.ccache android-build-fedora
+docker run -it --rm -v $1:/home/$(id -un)/DivestOS/Builds -v divestos_ccache:/home/$(id -un)/.ccache -v divestos_repo:/home/$(id -un)/DivestOS android-build-fedora
