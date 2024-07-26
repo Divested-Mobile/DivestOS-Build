0408730f50175cf1bbe6050509d79f32d8085c01
ryneeverett <ryneeverett@gmail.com>
2024-07-26 17:24:32 -0400

    Container: Correct build path

--- Container/run-image-podman.sh
+++ Container/run-image-podman.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mkdir -p $1
-podman run -it --rm --user=$(id -un) --workdir="/home/$(id -un)" --entrypoint="/bin/bash" -v $1:/home/$(id -un)/android -v divestos_ccache:/home/$(id -un)/.ccache android-build-fedora
+podman run -it --rm --user=$(id -un) --workdir="/home/$(id -un)" --entrypoint="/bin/bash" -v $1:/home/$(id -un)/DivestOS/Builds -v divestos_ccache:/home/$(id -un)/.ccache android-build-fedora
