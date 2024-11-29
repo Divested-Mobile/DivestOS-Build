cef79967f6eb15e6ee4770a4b106507eee990498
Tavi <tavi@divested.dev>
2024-11-29 15:10:50 -0500

    Fixup
    run tested:
    taimen/4.4, fajita/4.9, flame/4.14, bluejay/5.10
    
    Signed-off-by: Tavi <tavi@divested.dev>

--- Scripts/LineageOS-20.0/CVE_Patchers/android_kernel_google_gs101_private_gs-google.sh
+++ Scripts/LineageOS-20.0/CVE_Patchers/android_kernel_google_gs101_private_gs-google.sh
@@ -1270,7 +1270,6 @@ git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53057/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53060/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53061/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53066/5.10/0003.patch
-git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53091/^6.12/0001.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53101/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2021-47195/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2021-47472/5.10/0007.patch
@@ -1284,7 +1283,7 @@ git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-27425/5.10/0004.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-35812/5.10/0004.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-42104/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-49881/5.10/0002.patch
-editKernelLocalversion "-dos.p1284"
+editKernelLocalversion "-dos.p1283"
 else echo "kernel_google_gs101_private_gs-google is unavailable, not patching.";
 fi;
 cd "$DOS_BUILD_BASE"
--- Scripts/LineageOS-20.0/CVE_Patchers/android_kernel_google_gs201_private_gs-google.sh
+++ Scripts/LineageOS-20.0/CVE_Patchers/android_kernel_google_gs201_private_gs-google.sh
@@ -1268,7 +1268,6 @@ git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53057/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53060/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53061/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53066/5.10/0003.patch
-git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53091/^6.12/0001.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-53101/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2021-47195/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2021-47472/5.10/0007.patch
@@ -1282,7 +1281,7 @@ git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-27425/5.10/0004.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-35812/5.10/0004.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-42104/5.10/0003.patch
 git apply $DOS_PATCHES_LINUX_CVES/CVE-2024-49881/5.10/0002.patch
-editKernelLocalversion "-dos.p1282"
+editKernelLocalversion "-dos.p1281"
 else echo "kernel_google_gs201_private_gs-google is unavailable, not patching.";
 fi;
 cd "$DOS_BUILD_BASE"
